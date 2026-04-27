27cc24fbc4b06a1cf802cc60023b237f0f4877b2
Will Pelech <wpelech23@gmail.com>
2026-04-27 18:17:41 -0400

    adding qwen for local support

--- agent/agent.py
+++ agent/agent.py
@@ -1,11 +1,13 @@
 from google import genai 
-from .client import LLMClient, GemeniClient
+from .client import LLMClient, GemeniClient, QwenClient
 
 def buildClient(config:dict)->LLMClient:
     provider = config["provider"]
     model = config["model"]
     if provider == "Google": 
         return GemeniClient(model)
+    elif provider == "Qwen":
+        return QwenClient(model)
     raise ValueError(f"Unsupported valueError")
 
 def callModel():
